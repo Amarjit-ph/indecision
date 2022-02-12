90b0dbdf8791ae2a9eabf56c63f0928412ac6e81
Amarjit Pheiroijam <Amarjitp9@gmail.com>
2022-02-12 11:20:31 +0530

    Update IndecisionApp.js

--- src/Components/IndecisionApp.js
+++ src/Components/IndecisionApp.js
@@ -93,7 +93,7 @@ export default class IndecisionApp extends React.Component {
 
     /// RENDER
     render() {
-        const subtitle = "Pay Attention to Your Emotions & trust the randomness of Binary system";
+        const subtitle = "Pay Attention to Your Emotions & trust the randomness of Computer systems";
         //const options = ["One thing", "Two thing", "Three things"];
         return (
             <div>
